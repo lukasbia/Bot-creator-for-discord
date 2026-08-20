d2bca72a6a99f74c1a2be54a7a5b2eaf7554cda0
lukasbia <stodomingokylijahnver@gmail.com>
2026-08-20 13:51:14 +0800

    Create Botdetailpage.tsx

---
+++ Botdetailpage.tsx
@@ -0,0 +1,8 @@
+<header>{bot.name} <button>Add to Server</button></header>
+<div className="flex gap-2">
+  <button>Overview</button>
+  <button>Scripts</button>
+  <button>Variables</button>
+  <button>Settings</button>
+</div>
+// + modals for script editor, variable creator, etc.
