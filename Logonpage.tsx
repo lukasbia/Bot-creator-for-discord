9b1d083f8f10f3e737a7418de4bbbf595c7dd618
lukasbia <stodomingokylijahnver@gmail.com>
2026-08-20 13:50:04 +0800

    Create Logonpage.tsx

---
+++ Logonpage.tsx
@@ -0,0 +1,8 @@
+<div className="min-h-screen flex flex-col items-center justify-center">
+  <div className="w-20 h-20 bg-ios-tint rounded-ios-xl">
+    <Bot className="w-10 h-10 text-white" />
+  </div>
+  <h1>CBScript</h1>
+  <p>Bot Designer for Discord</p>
+  <button>Continue with Discord</button>
+</div>
